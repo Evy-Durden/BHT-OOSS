6624ab0c83ab1604979759d5be941a39e43797a5
Evy-Durden <lol@lol.com>
2025-05-22 15:38:35 +0200

    added more functions and fleshed out main

--- ESA-PyGame/snake_game.py
+++ ESA-PyGame/snake_game.py
@@ -3,6 +3,18 @@ import sys
 import time
 import random
 
+# dictionary for game state
+
+game_state = {'score': 0, 
+              'running': True, 
+              'snake_pos': [100, 50], 
+              'snake_body': [[100,50], [90, 50], [80, 50]],
+              'direction': 'RIGHT',
+              'change_drctn': 'RIGHT',
+              'prey_pos': [100, 100], 
+              'prey_spawn': True}
+
+
 
 # define colors - rgb
 black = pygame.Color(0, 0, 0)
@@ -33,42 +45,67 @@ pygame.display.set_caption('ESA2 Snake Game')
 # FPF frames per second controller
 fps_controller = pygame.time.Clock()
 
-running = True
+# dictionary for gfx
 
-# game variables
-# snake position - array of two values needed for the rect object to draw the snake onto the surface (left, top)
-snake_pos = [100, 50]
+gfx = {'black': black,
+       'white': white,
+       'red': red,
+       'green': green,
+       'blue': blue,
+       'grid_start_x': grid_start_x,
+       'grid_start_y': grid_start_y,
+       'grid_end_x': grid_end_x,
+       'grid_end_y': grid_end_y,
+       'screen': screen,
+       'fps_controller': fps_controller}
 
-# snake body intial size
-snake_body = [[100,50], [90, 50], [80, 50]]
 
-# prey the snake eats
-# using random.randrange to determine the new position of the prey
-prey_pos = [random.randrange(1, (window_size_x//10)) * 10, random.randrange(1, (window_size_y//10)) * 10]
-prey_spawn = True
-
-# direction of the snake
-direction = 'RIGHT'
-change_drktn = direction
-
-# intialize score
-score = 0
-
+# functions
 
 """
 display the current score
 
 """
 def show_score():
-    global score
+    global game_state
     # create font object
     score_font = pygame.font.SysFont('comic sans', 25)
     # render font object on surface
-    score_surface = score_font.render(f'Score: {score}', True, white)
+    score_surface = score_font.render(f'Score: {game_state['score']}', True, white)
     score_rect = score_surface.get_rect(topleft =(10, 10))
 
-    screen.blit(score_surface, score_rect)
+    gfx['screen'].blit(score_surface, score_rect)
 
+
+
+"""
+handling the snake
+
+"""
+def handle_snake():
+    global game_state, gfx
+
+    # moving the snake
+    if game_state['direction'] == 'UP':
+        game_state['snake_pos'][1] -= 10
+    if game_state['direction'] == 'DOWN':
+        game_state['snake_pos'][1] += 10
+    if game_state['direction'] == 'LEFT':
+        game_state['snake_pos'][0] -= 10
+    if game_state['direction'] == 'RIGHT':
+        game_state['snake_pos'][0] += 10
+
+    # growing mechanism of the snake
+    game_state['snake_body'].insert(0, list(game_state['snake_pos']))
+    if game_state['snake_pos'][0] == game_state['prey_pos'][0] and game_state['snake_pos'][1] == game_state['prey_pos'][1]:
+        game_state['score'] += 10
+        game_state['prey_spawn'] = False
+    else:
+        game_state['snake_pos'].pop()
+
+    if not game_state['prey_spawn']:
+        calc_prey_pos()
+        game_state['prey_spawn'] = True
 
 
 """
@@ -76,21 +113,52 @@ calculate new position of prey
 
 """
 def calc_prey_pos():
-    pass
+
+    global game_state, gfx
+
+    game_state['prey_pos'] = [random.randrange(1, (game_state['grid_end_x']//10)) * 10, 
+                              random.randrange(game_state['grid_start_y'], (game_state['grid_end_y']//10)) * 10]
 
 """
 game over function
 
 """
 def game_over():
-    pass
+
+    global game_state, gfx
+
+    g_o_font = pygame.font.SysFont('comic sans', 40)
+    g_o_surface = g_o_font.render(f'Game Over - You Died! \n Total Score: {game_state['score']}')
+    g_o_rect = g_o_surface.get_rect(midtop= (game_state['game_end_x']//2, game_state['game_end_y']//2))
+
+    gxf['screen'].fill(gfx['black'])
+    gfx['screen'].blit(g_o_surface, g_o_rect)
+    pygame.display.flip()
+    time.sleep(3)
+    pygame.quit()
+    sys.exit()
+    
 
 """
-populate screen
+populate screen with drawings
 
 """
 def render_game():
-    pass
+    global game_state, gfx
+
+    game_state['screen'].fill(gfx['black'])
+
+    # render snake
+    for position in game_state['snake_body']:
+        snake_rect = pygame.Rect(position[0], position[1], 10, 10)
+        pygame.draw.rect(game_state['screen'], game_state['blue'], snake_rect)
+    
+    # render prey
+    prey_rect = pygame.Rect(game_state['prey_pos'][0], game_state['prey_pos'][1], 10, 10)
+    pygame.draw.rect(game_state['screen'], game_state['green'], prey_rect)
+
+    pygame.display.flip()
+
 
 """
 main function for executing the game loop
@@ -98,19 +166,55 @@ main function for executing the game loop
 """
 def main():
 
-    global running, fps_controller
+    global game_state, gfx
 
-    while running:
+    # game loop
+    while game_state['running']:
 
+        # handling the key events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        show_score()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    game_state['change_drctn'] = 'UP'
+                if event.key == pygame.K_DOWN:
+                    game_state['change_drctn'] = 'DOWN'
+                if event.key == pygame.K_LEFT:
+                    game_state['change_drctn'] = 'LEFT'
+                if event.key == pygame.K_RIGHT:
+                    game_state['change_drctn'] = 'RIGHT'
+        
+        # in case two keys are pressed simultaneously
+        if game_state['change_drctn'] and game_state['direction'] != 'DOWN':
+            game_state['direction'] = 'UP'
+        if game_state['change_drctn'] == 'DOWN' and game_state['direction'] != 'UP':
+            game_state['direction'] = 'DOWN'
+        if game_state['change_drctn'] == 'LEFT' and game_state['direction'] != 'RIGHT':
+            game_state['direction'] = 'LEFT'
+        if game_state['change_drctn'] == 'RIGHT' and game_state['direction'] != 'LEFT':
+            game_state['direction'] = 'RIGHT'
 
-        pygame.display.flip()
+        handle_snake()
 
-        fps_controller.tick(60)
+        render_game()
+
+        # game over conditions
+        # snake is getting out of bounds
+        if game_state['snake_pos'][0] < 0 or game_state['snake_pos'][0] > gfx['grid_end_x']:
+            game_over()
+        if game_state['snake_pos'][1] < gfx['grid_start_y'] or game_state['snake_pos'][1] > gfx['grid_end_y']:
+            game_over()
+
+        # snake bites itself
+        for parts in game_state['snake_body']:
+            if game_state['snake_pos'][0] == parts[0] and game_state['snake_pos'][1] == parts[1]:
+                game_over()
+
+        pygame.display.update()
+
+        gfx['fps_controller'].tick(60)
 
     pygame.quit()
 
